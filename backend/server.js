3897653a7b64146e1ec699f078690101b850a671
DangetiPardheevKrishna <pardheevkrishna@gmail.com>
2025-03-17 18:52:36 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -8,17 +8,20 @@ import cookieParser from "cookie-parser";
 import path from "path";
 import { app, io, server } from "./socket/socket.js";
 const PORT = process.env.PORT || 5000;
-// const __dirname = path.resolve();
-// app.use(express.static(path.join(__dirname, "/frontend/dist")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-// });
+const __dirname = path.resolve();
+
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 // app.get("/", (req, res) => {
 //   res.send("hello world");
 // });
